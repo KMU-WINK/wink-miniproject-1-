ec9f71e489c8b95423c2f2949f92fa076aaff76f
Emshs <min5090seo@gmail.com>
2024-08-24 11:30:12 +0900

    -

---
+++ frontend/src/Pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Bar from '../components/Bars';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <Bar />
+        </div>
+    );
+}
+
+export default LoginPage;
---
+++ frontend/src/Pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Bar from '../components/Bars';
+
+const Mainpage = () => {
+    return (
+        <div>
+            <Bar />
+        </div>
+    );
+}
+
+export default Mainpage;
